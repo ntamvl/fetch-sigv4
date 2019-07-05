d2d9bc42887101cb08f84cbef0b77e046849bbd5
Tam Nguyen <ntamvl@gmail.com>
2019-07-05 11:16:09 +0700

    add function fetchApiSigv4

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 "use strict";
 
-module.exports = function(msg) {
-  console.log(msg);
-};
+var aws4 = require("./aws4");
+var axios = require("axios");
+var AWS = require("aws-sdk");
+var url = require("url");
+
+// Examnple config
+// var config = {
+//   endpoint: "https://example.com/v1/subscription",
+//   method: "POST",
+//   data: { name: "Tam Nguyen", blog: "https://ntam.me" },
+//   accessKeyId: "",
+//   secretAccessKey: "",
+//   sessionToken: ""
+// }
+
+function fetchApiSigv4(config) {
+  var data = config["data"];
+  var endpointURL = url.parse(config["endpoint"]);
+  var method = config["method"];
+
+  let request = {
+    host: endpointURL.host,
+    method: method,
+    url: endpointURL.href,
+    data: data,
+    body: JSON.stringify(data),
+    path: endpointURL.path,
+    headers: {
+      "content-type": "application/json"
+    }
+  };
+
+  let signedRequest = aws4.sign(request, {
+    // assumes user has authenticated and we have called
+    // AWS.config.credentials.get to retrieve keys and
+    // session tokens
+    secretAccessKey: AWS.config.credentials.secretAccessKey,
+    accessKeyId: AWS.config.credentials.accessKeyId,
+    sessionToken: AWS.config.credentials.sessionToken
+  });
+
+  delete signedRequest.headers["Host"];
+  delete signedRequest.headers["Content-Length"];
+
+  let response = await axios(signedRequest);
+  return response;
+}
+
+module.exports = fetchApiSigv4;
